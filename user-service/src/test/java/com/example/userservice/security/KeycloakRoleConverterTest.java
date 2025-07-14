b92df856ec424aea3fdf508c69853afc4c60c125
firaszn <mohamedfiras.zighni@esprit.tn>
2025-07-14 12:09:51 +0100

    devops

--- user-service/src/test/java/com/example/userservice/security/KeycloakRoleConverterTest.java
+++ user-service/src/test/java/com/example/userservice/security/KeycloakRoleConverterTest.java
@@ -2,7 +2,6 @@ package com.example.userservice.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.security.oauth2.jwt.Jwt;
-import java.util.Collections;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +9,13 @@ class KeycloakRoleConverterTest {
     @Test
     void testConvertNotNull() {
         KeycloakRoleConverter converter = new KeycloakRoleConverter();
-        Jwt jwt = new Jwt("token", null, null, Map.of("alg", "none"), Collections.emptyMap());
+        Jwt jwt = new Jwt(
+            "token",
+            null,
+            null,
+            Map.of("alg", "none"),
+            Map.of("sub", "test-user")
+        );
         assertNotNull(converter.convert(jwt));
     }
 } 
